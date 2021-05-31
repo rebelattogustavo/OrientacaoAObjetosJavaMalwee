e90c34390dde6cb2cbfa8f070571bdfa1e54baa5
rebelattogustavo <gu.zapella@gmail.com>
2021-05-31 16:38:57 -0300

    Definição da classe pessoa e Endereço

---
+++ src/br/com/senai/pessoa/Endereco.java
@@ -0,0 +1,29 @@
+package br.com.senai.pessoa;
+
+public class Endereco {
+	//ATRIBUTOS
+	private String cidade;
+	private String estado;
+	private String pais;
+	
+	//METODOS
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+}
---
+++ src/br/com/senai/pessoa/ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+package br.com.senai.pessoa;
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
